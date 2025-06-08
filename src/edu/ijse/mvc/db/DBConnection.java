48e33a8256216234463a8e5c2a53e3ed39d17b84
AnupaBud99 <anupabuddimakumbalathara@gmail.com>
2025-06-08 12:52:55 +0530

    ItemModel save Item with total flow

--- src/edu/ijse/mvc/db/DBConnection.java
+++ src/edu/ijse/mvc/db/DBConnection.java
@@ -19,7 +19,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "password");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "samudi12$");
     }
 
     public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
